12a9ac7459b6dc2caea699a32c43f82c0893b9e6
elishamutang <elishamutang@outlook.com>
2025-09-14 21:07:22 +0800

    update: Added commentary.

--- lib/services/todo_datasource.dart
+++ lib/services/todo_datasource.dart
@@ -1,5 +1,8 @@
 import 'package:todo_list/models/todo.dart';
 
+//  Abstract class IDataSource defines what operations can be done instead of how they are implemented.
+//  It essentially de-couples the app's business logic and UI from the details of the datasource.
+
 abstract class IDataSource {
   Future<List<Todo>> browse();
   Future<bool> add(Todo model);
